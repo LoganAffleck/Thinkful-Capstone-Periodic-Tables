7917967c523cafe95bdd8cbd8c73a1fd140c754c
J. Logan Affleck <affleck.logan@gmail.com>
2021-08-05 23:58:19 -0600

    e2e updates are working!

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,21 @@
+const service = require('./reservations.service')
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const data = await service.list();
+  res.json({ data });
+}
+
+async function create(req, res) {
+  const {reservation} = req.body.data;
+  const data = await service.create(reservation);
+  res.json({ data });
 }
 
 module.exports = {
-  list,
+  list: [asyncErrorBoundary(list)],
+  create: [asyncErrorBoundary(create)]
 };
